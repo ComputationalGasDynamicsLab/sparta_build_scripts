8f6e1a493c42cc53f11c2cdb3473993eb271a5c5
Chonglin Zhang <zhangchonglin@gmail.com>
2023-11-18 01:09:12 -0600

    update build script

--- centos/build_sparta.sh
+++ centos/build_sparta.sh
@@ -2,8 +2,11 @@ export buildroot=$PWD
 export srcroot=$buildroot/../
 export installroot=$buildroot/../install
 
-cmake $srcroot/cmake \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$installroot \
-        -DSPARTA_MACHINE=stokes
+cmake $srcroot/cmake
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_COMPILER=mpicxx \
+      -DCMAKE_INSTALL_PREFIX=$installroot \
+      -DBUILD_JPEG=ON \
+      -DBUILD_PNG=ON \
+      -DSPARTA_MACHINE=stokes
 make -j4 install
